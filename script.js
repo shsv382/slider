3678fd53ce330a2867035dc9db0d6bdbf64d7ea5
shsv382 <shsv382@gmail.com>
2017-08-17 00:11:15 +0400

    Added sliding without slow fadeIn/Out

--- script.js
+++ script.js
@@ -1,3 +1,50 @@
 $(document).ready(function(){
+	var image = document.getElementsByClassName("image")[0].firstElementChild;
+	var i = 0;
+	var circles = $(".circle");
+	var collection = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg"];
 	
+	var change = function() {
+		$("#image").fadeOut(300);
+		i++;
+		i = i % collection.length;
+		image.src = "images/" + collection[i];		
+		$("#image").fadeIn(300);
+		pointer();
+	}
+
+	var right = document.getElementById("right");
+	right.onclick = change;
+
+	var left = document.getElementById("left");
+	left.onclick = function() {
+		i--;
+		if (i < 0) i = i + collection.length;
+		image.src = "images/" + collection[i];
+		pointer();
+	}
+
+	var navi = document.getElementsByClassName("navi")[0];
+
+	navi.onclick = function(e) {
+		var target = e.target.closest(".circle");
+
+		if (target) {
+			i = target.dataset.src - 2;
+			change();
+		}
+
+		clearTimeout(timerID);
+		timerID = setInterval(change, 5000);
+	}
+
+	function pointer() {
+		for (var j = 0; j < collection.length; j++) circles[j].style.backgroundColor = "";
+		circles[i].style.backgroundColor = "#555";
+	}
+
+	pointer();
+
+	var timerID = setInterval(change, 5000);
+
 });
